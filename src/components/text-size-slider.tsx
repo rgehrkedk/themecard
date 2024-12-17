6cd6f9c573b56de381e8fb9263e64813aac50a2b
Rasmus <60469087+rgehrkedk@users.noreply.github.com>
2024-12-17 07:58:10 +0100

    Update3 text-size-slider.tsx

--- src/components/text-size-slider.tsx
+++ src/components/text-size-slider.tsx
@@ -27,10 +27,10 @@ export const TextSizeSlider = ({
 
         <div className="flex-1 relative h-6">
           {/* Background track */}
-          <div className="absolute w-full h-1 top-1/2 -translate-y-1/2 bg-slate-200 dark:bg-slate-700 rounded">
+          <div className="absolute w-full h-1 top-1/2 -translate-y-1/2 bg-slate-200 dark:bg-slate-700 rounded-full overflow-hidden">
             {/* Colored progress bar */}
             <div 
-              className={`absolute h-full left-0 rounded transition-all duration-300 ${
+              className={`absolute h-full left-0 rounded-full transition-all duration-300 ${
                 preferences.gradients 
                   ? `bg-gradient-to-r ${themes[activeTheme].primary}` 
                   : themes[activeTheme].solid
@@ -44,22 +44,33 @@ export const TextSizeSlider = ({
             type="range"
             min="0"
             max="4"
-            step="1"
+            step="0.1"
             value={textSize}
-            onChange={(e) => setTextSize(parseInt(e.target.value))}
+            onChange={(e) => setTextSize(parseFloat(e.target.value))}
             className={`
-              w-full h-6 appearance-none bg-transparent cursor-pointer rounded-lg
+              w-full h-6 appearance-none bg-transparent cursor-pointer
               [&::-webkit-slider-thumb]:appearance-none
               [&::-webkit-slider-thumb]:w-4
               [&::-webkit-slider-thumb]:h-4
               [&::-webkit-slider-thumb]:rounded-full
               [&::-webkit-slider-thumb]:bg-white
               [&::-webkit-slider-thumb]:shadow-md
+              [&::-webkit-slider-thumb]:border-2
+              [&::-webkit-slider-thumb]:border-slate-200
+              [&::-webkit-slider-thumb]:transition-all
+              [&::-webkit-slider-thumb]:duration-150
+              [&::-webkit-slider-thumb]:hover:scale-110
               [&::-moz-range-thumb]:w-4
               [&::-moz-range-thumb]:h-4
               [&::-moz-range-thumb]:rounded-full
               [&::-moz-range-thumb]:bg-white
-              [&::-moz-range-thumb]:border-none
+              [&::-moz-range-thumb]:shadow-md
+              [&::-moz-range-thumb]:border-2
+              [&::-moz-range-thumb]:border-slate-200
+              [&::-moz-range-thumb]:transition-all
+              [&::-moz-range-thumb]:duration-150
+              [&::-moz-range-thumb]:hover:scale-110
+              focus:outline-none
             `}
             style={{
               WebkitAppearance: 'none',
